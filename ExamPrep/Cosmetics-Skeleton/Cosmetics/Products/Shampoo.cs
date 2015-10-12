a31e4c38b466f578e01438df3672b2e421794b8c
go6koy <go6koy@gmail.com>
2015-10-12 22:32:26 +0300

    3rd ExamPrepProblem Solved

---
+++ ExamPrep/Cosmetics-Skeleton/Cosmetics/Products/Shampoo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetics.Common;
+using Cosmetics.Contracts;
+
+namespace Cosmetics.Products
+{
+    class Shampoo:Product,IShampoo
+    {
+        public Shampoo(string name, string brand, decimal price, GenderType gender,uint milliliters,UsageType usage) 
+            : base(name, brand, price, gender)
+        {
+            this.Milliliters = milliliters;
+            this.Usage = usage;
+        }
+
+        public uint Milliliters { get; private set; }
+        public UsageType Usage { get; private set; }
+
+        public override string Print()
+        {
+            StringBuilder output=new StringBuilder(base.Print());
+            output.AppendLine(String.Format("  * Quantity: {0} ml", this.Milliliters));
+            output.Append(String.Format("  * Usage: {0} ", this.Usage));
+            return output.ToString();
+        }
+    }
+}
---
+++ ExamPrep/Cosmetics-Skeleton/Cosmetics/Products/ShoppingCart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetics.Contracts;
+
+namespace Cosmetics.Products
+{
+    class ShoppingCart:IShoppingCart
+    {
+        private readonly List<IProduct> productsToBuy; 
+        public ShoppingCart()
+        {
+            this.productsToBuy=new List<IProduct>();
+        }
+        
+        public void AddProduct(IProduct product)
+        {
+            this.productsToBuy.Add(product);
+        }
+
+        public void RemoveProduct(IProduct product)
+        {
+            this.productsToBuy.Remove(product);
+        }
+
+        public bool ContainsProduct(IProduct product)
+        {
+            return this.productsToBuy.Contains(product);
+        }
+
+        public decimal TotalPrice()
+        {
+            return this.productsToBuy.Sum(p => p.Price);
+        }
+    }
+}
